64d1b5c64e6d3f467d4b160a06e84288a0a48678
Григанська Олександра Олександрівна <ipz235_goo@student.ztu.edu.ua>
2025-02-02 22:09:07 +0200

    Add file

---
+++ main.py
@@ -0,0 +1,30 @@
+a = int(input("a:"))
+b = int(input("b:"))
+c = float(input("c:"))
+d = float(input("d:"))
+
+sum = a + b
+sub = a - b
+mult = a * b
+div = a / b
+ed = c ** d
+id = c // d
+rd = c % d
+
+list = [sum, sub, mult, div, ed, id, rd]
+print(list)
+print(len(list))
+
+for x in list:
+    if x % 2 == 0 and x != 0:
+        print(x)
+
+for x in range(len(list)):
+    if x % 2 == 0 and x != 0:
+        print(x)
+
+list[1],list[4] = list[4],list[1]
+print(list)
+
+name = str(input("Ім'я та прізвище: "))
+print(name, "виконала лабораторну роботу №1\nЗасвоїла основні правила роботи з Pathon\nОзнайомилась з алгоритмами послідовної (лінійної) структури, з процедурами запуску програм, які реалізують ці алгоритми.")
