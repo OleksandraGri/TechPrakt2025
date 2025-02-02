a91b792e1bf71c7749560a5b2a51b471032b0a8c
Григанська Олександра Олександрівна <ipz235_goo@student.ztu.edu.ua>
2025-02-02 22:35:47 +0200

    Fix: changed reserved variable names

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ b = int(input("b:"))
 c = float(input("c:"))
 d = float(input("d:"))
 
-sum = a + b
+sum_result = a + b
 sub = a - b
 mult = a * b
 div = a / b
@@ -11,20 +11,20 @@ ed = c ** d
 id = c // d
 rd = c % d
 
-list = [sum, sub, mult, div, ed, id, rd]
-print(list)
-print(len(list))
+results_list = [sum_result, sub, mult, div, ed, id, rd]
+print(results_list)
+print(len(results_list))
 
-for x in list:
+for x in results_list:
     if x % 2 == 0 and x != 0:
         print(x)
 
-for x in range(len(list)):
+for x in range(len(results_list)):
     if x % 2 == 0 and x != 0:
         print(x)
 
-list[1],list[4] = list[4],list[1]
-print(list)
+results_list[1],results_list[4] = results_list[4],results_list[1]
+print(results_list)
 
 name = str(input("Ім'я та прізвище: "))
 print(name, "виконала лабораторну роботу №1\nЗасвоїла основні правила роботи з Pathon\nОзнайомилась з алгоритмами послідовної (лінійної) структури, з процедурами запуску програм, які реалізують ці алгоритми.")
